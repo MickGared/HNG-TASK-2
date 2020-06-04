fab80d8362415a99a2c1a3950830798a5c14a959
MickB <66332295+MickGared@users.noreply.github.com>
2020-06-04 21:04:13 +0100

    Update Balogun Micheal.js

--- Balogun Micheal.js
+++ Balogun Micheal.js
@@ -1,7 +1,10 @@
-<<script>>
+let FullName = 'Balogun Ayobami';
+let id = 'HNG-05997';
+let lang = 'JavaScript';
+let Email = 'mickgared9@mail.com';
+Console.log('Hello World, this is ${FullName} with ${id}, and I am using ${lang} for stage 2 task.${Email};
 
-function myFunction(FullName, id, myLanguage, myEmail) {
+
+
+
 	
-	return "Hello World, this is ${FullName} with ${id}, and I am using ${myLanguage} for stage 2 task.${Email};
-}
-Console.log (myFunction("Micheal Balogun", "HNG-0997", "Javascript", "mickgared9@gmail.com));   
