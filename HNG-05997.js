1d148a3f2ee16080aee17d9aa460416ea0c11b66
MickB <66332295+MickGared@users.noreply.github.com>
2020-06-04 23:42:10 +0100

    Update HNG-05997.js

--- HNG-05997.js
+++ HNG-05997.js
@@ -5,7 +5,7 @@ let Email = " mickgared9@mail.com";
 
 function Return() {
 
-      output = console.log("Hello World, this is ${FullName} with ${id}, and I am using ${lang} for stage 2 task.${Email}");
+      output = console.log(`Hello World, this is ${FullName} with ${id}, and I am using ${lang} for stage 2 task.${Email}`);
 
       return output;
 }
