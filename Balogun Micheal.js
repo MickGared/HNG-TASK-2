7ed871efeb88926afd48b7c48e3171d6ddcaac7e
MickB <66332295+MickGared@users.noreply.github.com>
2020-06-04 20:47:02 +0100

    Balogun Micheal.js

---
+++ Balogun Micheal.js
@@ -0,0 +1,7 @@
+<<script>>
+
+function myFunction(FullName, id, myLanguage, myEmail) {
+	
+	return "Hello World, this is ${FullName} with ${id}, and I am using ${myLanguage} for stage 2 task.${Email};
+}
+Console.log (myFunction("Micheal Balogun", "HNG-0997", "Javascript", "mickgared9@gmail.com));   
